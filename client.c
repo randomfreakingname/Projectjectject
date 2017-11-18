f65866b5cd742c04486c7ef7a4067d5f232919fc
yungwiche <le.minhhoang@outlook.com>
2017-11-18 10:21:20 +0700

    MakeCommand

--- client.c
+++ client.c
@@ -8,9 +8,9 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-typedef struct{
-  char code[20];
-  char params[2][30];
+typedef struct {
+        char code[20];
+        char params[2][30];
 } command;
 
 
@@ -26,174 +26,190 @@ void showMenuLogin();
 void showLoginForm();
 void makeLoginCommand(char*command,char *username, char *password);
 void makeSignupCommand(char*command,char *username, char *password);
+void makeCommand(char* command, char* code, char* param1, char* param2);
 void showSignupForm();
 void showMenuFunction();
 command convertReponseToCommand(char *response);
 
 int main(){
-    sockfd = socket(AF_INET,SOCK_STREAM,0);
-    struct sockaddr_in serverAddr;
+        sockfd = socket(AF_INET,SOCK_STREAM,0);
+        struct sockaddr_in serverAddr;
 
-    serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(SERV_PORT);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(SERV_PORT);
+        serverAddr.sin_addr.s_addr = INADDR_ANY;
 
-    if(connect(sockfd,(struct sockaddr*)&serverAddr,sizeof(serverAddr))!= 0){
-		printf("Loi: Ket noi den server that bai\n");
-		exit(1);
-	}
-    showMenuLogin();
-    close(sockfd);
+        if(connect(sockfd,(struct sockaddr*)&serverAddr,sizeof(serverAddr))!= 0) {
+                printf("Loi: Ket noi den server that bai\n");
+                exit(1);
+        }
+        showMenuLogin();
+        close(sockfd);
 }
 
 void showMenuLogin(){
-    int choice;
-    char c;
-    while (1){
-        choice = 0;
-        printf("----Welcome-----\n");
-        printf("Please login or signup to use our service\n");
-        printf("1.Login\n");
-        printf("2.Signup\n");
-        printf("3.Exit\n");
-        printf("Your choice:");
-        while (choice == 0) {
-            if(scanf("%d",&choice) < 1){
+        int choice;
+        char c;
+        while (1) {
                 choice = 0;
-            }
-            if(choice < 1 || choice > 3){
-                choice = 0;
-                printf("Invalid choice!\n");
-                printf("Enter again:");
-            }
-            while((c = getchar())!='\n');
-        }
+                printf("----Welcome-----\n");
+                printf("Please login or signup to use our service\n");
+                printf("1.Login\n");
+                printf("2.Signup\n");
+                printf("3.Exit\n");
+                printf("Your choice:");
+                while (choice == 0) {
+                        if(scanf("%d",&choice) < 1) {
+                                choice = 0;
+                        }
+                        if(choice < 1 || choice > 3) {
+                                choice = 0;
+                                printf("Invalid choice!\n");
+                                printf("Enter again:");
+                        }
+                        while((c = getchar())!='\n') ;
+                }
 
-        switch (choice) {
-            case 1:
-                showLoginForm();
-                if (recv(sockfd, serverResponse, MAXLINE, 0) == 0){
-                    perror("The server terminated prematurely");
-                    exit(4);
+                switch (choice) {
+                case 1:
+                        showLoginForm();
+                        if (recv(sockfd, serverResponse, MAXLINE, 0) == 0) {
+                                perror("The server terminated prematurely");
+                                exit(4);
+                        }
+                        cmd = convertReponseToCommand(serverResponse);
+                        if (atoi(cmd.code) == 200) {
+                                showMenuFunction();
+                        }
+                        else {
+                                printf("%s\n",cmd.params[0]);
+                        }
+                        break;
+                case 2:
+                        showSignupForm();
+                        break;
+                default:
+                        exit(0);
                 }
-                cmd = convertReponseToCommand(serverResponse);
-                if (atoi(cmd.code) == 200) {
-                    showMenuFunction();
-                }
-                else {
-                    printf("%s\n",cmd.params[0]);
-                }
-                break;
-            case 2:
-                showSignupForm();
-                break;
-            default:
-                exit(0);
         }
-    }
 }
 
 void showLoginForm(){
-    char password[30];
-    char command[100];
-    printf("Username:\n");
-    gets(username);
-    printf("Password:\n");
-    gets(password);
-    makeLoginCommand(command,username, password);
-    send (sockfd,command,sizeof(command),0);
+        char password[30];
+        char command[100];
+        printf("Username:\n");
+        gets(username);
+        printf("Password:\n");
+        gets(password);
+        makeCommand(command,"LOGIN", username, password);
+        send (sockfd,command,sizeof(command),0);
 }
 
 void makeLoginCommand(char *command, char *username, char *password){
-    strcpy(command,"LOGIN|");
-    strcat(command,username);
-    strcat(command,"|");
-    strcat(command,password);
-    strcat(command,"|");
+        strcpy(command,"LOGIN|");
+        strcat(command,username);
+        strcat(command,"|");
+        strcat(command,password);
+        strcat(command,"|");
+}
+
+void makeCommand(char* command, char* code, char* param1, char* param2){
+        strcpy(command,code);
+        strcat(command, "|");
+        if (param1 != NULL) {
+                strcat(command,param1);
+                strcat(command,"|");
+        }
+        if (param2 != NULL) {
+                strcat(command,param2);
+                strcat(command,"|");
+        }
 }
 
 
 void showSignupForm(){
-    char password[30];
-    char command[100];
-    printf("Username:\n");
-    gets(username);
-    printf("Password:\n");
-    gets(password);
-    makeSignupCommand(command,username, password);
-    send (sockfd,command,sizeof(command),0);
+        char password[30];
+        char command[100];
+        printf("Username:\n");
+        gets(username);
+        printf("Password:\n");
+        gets(password);
+        makeCommand(command,"SIGNUP", username, password);
+        send (sockfd,command,sizeof(command),0);
 }
 
 void makeSignupCommand(char *command, char *username, char *password){
-    strcpy(command,"SIGNUP|");
-    strcat(command,username);
-    strcat(command,"|");
-    strcat(command,password);
-    strcat(command,"|");
+        strcpy(command,"SIGNUP|");
+        strcat(command,username);
+        strcat(command,"|");
+        strcat(command,password);
+        strcat(command,"|");
 }
 
 command convertReponseToCommand(char *response){
-	char copy[100];
-	strcpy(copy,response);
-	command cmd;
-	char *token;
-	int i = 0;
-	token = strtok(copy, "|");
-	strcpy(cmd.code, token);
-	while(token != NULL){
-		token = strtok(NULL, "|");
-		if(token != NULL){
-			strcpy(cmd.params[i++],token);
-		}
-	}
-	return cmd;
+        char copy[100];
+        strcpy(copy,response);
+        command cmd;
+        char *token;
+        int i = 0;
+        token = strtok(copy, "|");
+        strcpy(cmd.code, token);
+        while(token != NULL) {
+                token = strtok(NULL, "|");
+                if(token != NULL) {
+                        strcpy(cmd.params[i++],token);
+                }
+        }
+        return cmd;
 }
 
 
 void showMenuFunction(){
-    int choice;
-    char c;
-    while (1){
-        choice = 0;
-        printf("Hello %s\n", username);
-        printf("Here is your function:\n");
-        printf("1.Create new folder\n");
-        printf("2.See your folder\n");
-        printf("3.Upload file\n");
-        printf("4.Delete file\n");
-        printf("5.Logout\n");
-        printf("Your choice:");
-        while (choice == 0) {
-            if(scanf("%d",&choice) < 1){
+        int choice;
+        char c;
+        while (1) {
                 choice = 0;
-            }
-            if(choice < 1 || choice > 5){
-                choice = 0;
-                printf("Invalid choice!\n");
-                printf("Enter again:");
-            }
-            while((c = getchar())!='\n');
-        }
+                printf("Hello %s\n", username);
+                printf("Here is your function:\n");
+                printf("1.Create new folder\n");
+                printf("2.See your folder\n");
+                printf("3.Upload file\n");
+                printf("4.Delete file\n");
+                printf("5.Logout\n");
+                printf("Your choice:");
+                while (choice == 0) {
+                        if(scanf("%d",&choice) < 1) {
+                                choice = 0;
+                        }
+                        if(choice < 1 || choice > 5) {
+                                choice = 0;
+                                printf("Invalid choice!\n");
+                                printf("Enter again:");
+                        }
+                        while((c = getchar())!='\n') ;
+                }
 
-        switch (choice) {
-            case 1:
-                printf("code for create new folder here\n");
+                switch (choice) {
+                case 1:
+                        printf("code for create new folder here\n");
+                        break;
+                case 2:
+                        printf("code for see folder here\n");
+                        break;
+                case 3:
+                {
+
+                }
                 break;
-            case 2:
-                printf("code for see folder here\n");
-                break;
-            case 3:
-                printf("code for upfile here\n");
-                break;
-            case 4:
-                printf("code for delete file here\n");
-                break;
-            case 5:
-                printf("code for logout here\n");
-                break;
+                case 4:
+                        printf("code for delete file here\n");
+                        break;
+                case 5:
+                        printf("code for logout here\n");
+                        break;
+                }
+                if (choice == 5) {
+                        break;
+                }
         }
-        if (choice == 5){
-            break;
-        }
-    }
 }
--- server.c
+++ server.c
@@ -23,16 +23,21 @@ unsigned int port=3306;
 static char *unix_socket= NULL;
 unsigned int flag=0;
 MYSQL *conn;
+int listenSock, connectSock, n;
+pid_t pid;
+char request[MAXLINE];
+struct sockaddr_in serverAddr, clientAddr;
+socklen_t clilen;
 
-typedef struct{
-  char username[30];
-  int id;
+typedef struct {
+        char username[30];
+        int id;
 } User;
 
 
-typedef struct{
-  char code[20];
-  char params[2][30];
+typedef struct {
+        char code[20];
+        char params[2][30];
 } command;
 
 User currentUser;
@@ -41,152 +46,162 @@ void sig_chld(int singno);
 command convertRequestToCommand(char *request);
 char* processCommand(command cmd);
 int isUserExisted(char* username);
+command cmd;
 
 int main(){
-	conn=mysql_init(NULL);
-	if (!(mysql_real_connect(conn,host,user,pass,dbname,port,unix_socket,flag)))
-	{
-		printf("\nError: %s [%d]\n",mysql_error(conn), mysql_errno(conn));
-		exit(1);
-	}
-    currentUser.id = 0; //no current user
-	printf("Connection Successful\n");
-	pid_t pid;
-    int listenSock, connectSock, n;
-    char request[MAXLINE];
-    struct sockaddr_in serverAddr, clientAddr;
-    socklen_t clilen;
-	command cmd;
-    if((listenSock = socket(AF_INET,SOCK_STREAM,0)) < 0){
-		printf("Loi tao socket\n");
-        exit(1);
-	}
+        conn=mysql_init(NULL);
+        if (!(mysql_real_connect(conn,host,user,pass,dbname,port,unix_socket,flag)))
+        {
+                printf("\nError: %s [%d]\n",mysql_error(conn), mysql_errno(conn));
+                exit(1);
+        }
+        currentUser.id = 0; //no current user
+        printf("Connection Successful\n");
 
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverAddr.sin_port = htons(SERV_PORT);
+        if((listenSock = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+                printf("Loi tao socket\n");
+                exit(1);
+        }
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        serverAddr.sin_port = htons(SERV_PORT);
 
-    if(bind(listenSock,(struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0){
-		printf("Loi bind\n");
-        exit(2);
-	}
-    listen(listenSock,LISTENQ);
-    clilen = sizeof(clientAddr);
-    char* message;
-    while (1) {
-        connectSock = accept (listenSock, (struct sockaddr *) &clientAddr, &clilen);
-        if((pid=fork()) == 0){
-			close(listenSock);
-			while ((n = recv(connectSock, request, MAXLINE,0)) > 0)  {
- 				cmd = convertRequestToCommand(request);
-				message = processCommand(cmd);
-                send(connectSock,message,MAXLINE,0);
-			}
-            currentUser.id = 0;
-            printf("%d\n",currentUser.id );
-			close(connectSock);
-			exit(0);
-		}
-        signal(SIGCHLD,sig_chld);
-		close(connectSock);
-    }
+        if(bind(listenSock,(struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+                printf("Loi bind\n");
+                exit(2);
+        }
+        listen(listenSock,LISTENQ);
+        clilen = sizeof(clientAddr);
+        char* message;
+        while (1) {
+                connectSock = accept (listenSock, (struct sockaddr *) &clientAddr, &clilen);
+                if((pid=fork()) == 0) {
+                        close(listenSock);
+                        while ((n = recv(connectSock, request, MAXLINE,0)) > 0)  {
+                                cmd = convertRequestToCommand(request);
+                                message = processCommand(cmd);
+                                send(connectSock,message,MAXLINE,0);
+                        }
+                        currentUser.id = 0;
+                        printf("%d\n",currentUser.id );
+                        close(connectSock);
+                        exit(0);
+                }
+                signal(SIGCHLD,sig_chld);
+                close(connectSock);
+        }
 }
 
 
 
 void sig_chld(int singno){
-	pid_t pid;
-	int stat;
-	while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0)
-		printf("child %d terminated\n", pid);
-	return;
+        pid_t pid;
+        int stat;
+        while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0)
+                printf("child %d terminated\n", pid);
+        return;
 }
 
 command convertRequestToCommand(char *request){
-	char copy[100];
-	strcpy(copy,request);
-	command cmd;
-	char *token;
-	int i = 0;
-	token = strtok(copy, "|");
-	strcpy(cmd.code, token);
-	while(token != NULL){
-		token = strtok(NULL, "|");
-		if(token != NULL){
-			strcpy(cmd.params[i++],token);
-		}
-	}
-	return cmd;
+        char copy[100];
+        strcpy(copy,request);
+        command cmd;
+        char *token;
+        int i = 0;
+        token = strtok(copy, "|");
+        strcpy(cmd.code, token);
+        while(token != NULL) {
+                token = strtok(NULL, "|");
+                if(token != NULL) {
+                        strcpy(cmd.params[i++],token);
+                }
+        }
+        return cmd;
 }
 
 char* processCommand(command cmd){
-	MYSQL_RES *result;
-	MYSQL_ROW row;
-    char* message = malloc(MAXLINE*sizeof(char));
-	char query[1000];
-	if(strcmp(cmd.code,"LOGIN") == 0){
-	    int i;
-	    sprintf(query, "Select count(*),username,password,id FROM user WHERE username ='%s'",cmd.params[0]);
-	    if (mysql_query(conn, query)) {
-	        mysql_close(conn);
-	        strcpy(message,"401|Cant connect to database");
-            return message;
-	    }
-	    result = mysql_store_result(conn);
-	    if(result == NULL) {
-	        mysql_close(conn);
-            strcpy(message,"401|Cant execute query");
-            return message;
-	    }
-	    row = mysql_fetch_row(result);
-	    if (atoi(row[0])<1) {
-            strcpy(message,"401|Cant find username");
-	    } else {
-	    	if (strcmp(cmd.params[1],row[2])==0)
-	    	{
-                strcpy(message,"200|");
-                strcpy(currentUser.username,cmd.params[0]);
-                currentUser.id = atoi(row[2]);
-                printf("%s\n",currentUser.username );
-	    	} else {
-	    	    strcpy(message,"401|Wrong password");
-	    	}
-	    }
-        return message;
-	}
-	else if(strcmp(cmd.code,"SIGNUP") == 0){
-		if (isUserExisted(cmd.params[0])==1){
-			printf("lmao who cares haha xd\n");
-			return NULL;
-		}
-	    sprintf(query, "insert into user(username,password) values ('%s','%s')",cmd.params[0],cmd.params[1]);
-	    if (mysql_query(conn, query)) {
-	        mysql_close(conn);
-	        return NULL;
-	    }
-	}
+        MYSQL_RES *result;
+        MYSQL_ROW row;
+        char* message = malloc(MAXLINE*sizeof(char));
+        char query[1000];
+        if(strcmp(cmd.code,"LOGIN") == 0) {
+                int i;
+                sprintf(query, "Select count(*),username,password,id FROM user WHERE username ='%s'",cmd.params[0]);
+                if (mysql_query(conn, query)) {
+                        mysql_close(conn);
+                        strcpy(message,"401|Cant connect to database");
+                        return message;
+                }
+                result = mysql_store_result(conn);
+                if(result == NULL) {
+                        mysql_close(conn);
+                        strcpy(message,"401|Cant execute query");
+                        return message;
+                }
+                row = mysql_fetch_row(result);
+                if (atoi(row[0])<1) {
+                        strcpy(message,"401|Cant find username");
+                } else {
+                        if (strcmp(cmd.params[1],row[2])==0)
+                        {
+                                strcpy(message,"200|");
+                                strcpy(currentUser.username,cmd.params[0]);
+                                currentUser.id = atoi(row[2]);
+                                printf("%s\n",currentUser.username );
+                        } else {
+                                strcpy(message,"401|Wrong password");
+                        }
+                }
+                return message;
+        }
+        else if(strcmp(cmd.code,"SIGNUP") == 0) {
+                if (isUserExisted(cmd.params[0])==1) {
+                        printf("lmao who cares haha xd\n");
+                        return NULL;
+                }
+                sprintf(query, "insert into user(username,password) values ('%s','%s')",cmd.params[0],cmd.params[1]);
+                if (mysql_query(conn, query)) {
+                        mysql_close(conn);
+                        return NULL;
+                }
+        }else if (strcmp(cmd.code, "UPLOAD") == 0) {
+                printf("%s\n","Uploading file to server");
+                FILE* fptr = fopen(cmd.params[0], "wb");
+                if ((n = recv(connfd, buf, MAXLINE,0)) > 0)  {
+                        printf("Filename received from the client: %s\n", buf);
+                        fptr = fopen(buf, "wb");
+                        while() {
+                                fwrite(buf, n, 1, fptr);
+                                send(connfd, buf, n, 0);
+                        }
+                        printf("File transfering successful\n");
+                        fclose(fptr);
+                        break;
+                }
+                close(connfd);
+        }
 }
 
 int isUserExisted(char* username) {
-	MYSQL_RES *result;
-	MYSQL_ROW row;
-	char query[1000];
-	int i;
-    sprintf(query, "Select count(*) FROM user WHERE username ='%s'",username);
-    if (mysql_query(conn, query)) {
-        mysql_close(conn);
-        return 0;
-    }
-    result = mysql_store_result(conn);
-    if(result == NULL) {
-        mysql_close(conn);
-        return 0;
-    }
-    while ((row = mysql_fetch_row(result))) {
-    	if (atoi(row[0])<1) {
-    		return 0;
-    	} else {
-    		return 1;
-    	}
-    }
+        MYSQL_RES *result;
+        MYSQL_ROW row;
+        char query[1000];
+        int i;
+        sprintf(query, "Select count(*) FROM user WHERE username ='%s'",username);
+        if (mysql_query(conn, query)) {
+                mysql_close(conn);
+                return 0;
+        }
+        result = mysql_store_result(conn);
+        if(result == NULL) {
+                mysql_close(conn);
+                return 0;
+        }
+        while ((row = mysql_fetch_row(result))) {
+                if (atoi(row[0])<1) {
+                        return 0;
+                } else {
+                        return 1;
+                }
+        }
 }
